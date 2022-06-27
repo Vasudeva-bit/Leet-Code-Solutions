161ecd7bf3662ee15bae52fb19a4742d052c8fd1
Vasudeva-bit <venus.kilaru@gmail.com>
2022-06-27 22:12:10 +0530

    Added more solutions

---
+++ Easy/20. Valid Parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch:s){
+            if(ch == '{' || ch =='(' || ch == '[')
+                st.push(ch);
+            else if(ch == '}' || ch ==')' || ch == ']')
+                if(ch == '}' && !st.empty() && st.top() == '{')
+                    st.pop();
+                else if(ch == ')' && !st.empty() && st.top() == '(')
+                    st.pop();
+                else if(ch == ']' && !st.empty() && st.top() == '[')
+                    st.pop();
+                else
+                    return false;
+        }
+        return st.empty();
+    }
+};
